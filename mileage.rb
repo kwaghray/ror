f3986eee832b2a66d4fc093429f94d759d407bf9
kwaghray <kunalwaghray52@gmail.com>
2014-08-30 11:44:01 +0530

    added mileage file

---
+++ mileage.rb
@@ -0,0 +1,14 @@
+
+
+puts "please give the starting point in KM"
+name=gets
+puts "please give the ending point in KM"
+name1=gets
+puts "enter how many litres"
+name2=gets
+ a = (name1.to_i - name.to_i)/name2.to_i
+
+
+
+puts "the mileage is: #{a}" 
+
